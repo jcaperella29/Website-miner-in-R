7179a1fea5cb7a3ab1f37143a622c995f0295245
jcaperella29 <137432703+jcaperella29@users.noreply.github.com>
2024-03-24 21:54:58 -0400

    Update Webpage_mining .R
    cleaned up the script  by  removing unneeded lines of code

--- Webpage_mining .R
+++ Webpage_mining .R
@@ -18,8 +18,7 @@ url <- "https://link.springer.com/article/10.1007/s12282-024-01555-9"
 url_2<- "https://www.frontiersin.org/journals/oncology/articles/10.3389/fonc.2020.00120/full"
 urls<-c(url,url_2)
 
-text_test<-pdf_text(pdf_2)
-pdfs<-c(pdf,pdf_2)
+
 text_collector<-function(urls){
 texts<-list()
 
@@ -39,37 +38,34 @@ text_to_analyze<-text_collector(urls)
 
 
 
-# 3. We are going to analyze the State of the Union Addresses from 1934 to 2020. First, set your working directory to the location of your files.
 
 corp<-corpus(text_to_analyze)
-# 4. Create a corpus. A corpus is a structure for text analysis that retains the position of the words.
 
 
 
-article_corp <-corpus (text)
-article_corp
+
 
 
 
 
 #Take a look at a few statistics
 
-ndoc(article_corp) # document count
-docnames(article_corp) # unique document identifiers
-ntoken(article_corp) # tokens in each document
-ntype(article_corp) # types (unique tokens) in each document
-nsentence(article_corp) # sentences in each document
-summary(article_corp)# to get types, tokens, sentences
-summary(article_corp,5)
+ndoc(corp) # document count
+docnames(corp) # unique document identifiers
+ntoken(corp) # tokens in each document
+ntype(corp) # types (unique tokens) in each document
+nsentence(corp) # sentences in each document
+summary(corp)# to get types, tokens, sentences
+summary(corp,5)
 
 
 
 
 # 5. Tokenize your corpus
 
-article_toks <-tokens(article_corp)# It removes separators (whitespaces)but we can remove numbers,punctuation, symbols
+article_toks <-tokens(corp)# It removes separators (whitespaces)but we can remove numbers,punctuation, symbols
 print(article_toks)
-article_toks <- tokens(article_corp,  remove_punct = TRUE)
+article_toks <- tokens(corp,  remove_punct = TRUE)
 print(article_toks)
 
 
